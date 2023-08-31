a881563d0f58ffd40ec408faf5d01e058838cfaa
harshsamanpureiitm <143727190+harshsamanpureiitm@users.noreply.github.com>
2023-08-31 21:03:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,21 @@
 import streamlit as st
 st.write("Hello world!")
+# Python program to find the largest number among the three input numbers
+
+num1 = 10
+num2 = 14
+num3 = 12
+
+# uncomment following lines to take three numbers from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
